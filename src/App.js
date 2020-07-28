271d3e319d1b756c06ef4770af71e65c95b3cd48
Ester <esterfania@users.noreply.github.com>
2020-07-28 10:59:04 -0300

    ajuste do layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import Menu from './components/Menu'
+import BannerMain from './components/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json';
+import Carousel from './components/Carousel'
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <header>
+    <div style={{ background: '#2b2d42' }}>
+
       <Menu />
-    </header>
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+      <Footer />
+
+    </div>
   );
 }
 
